ccda44b36cf173bbb7f3221ed63df89809ad1125
Dmitry Kartsev <dkartsev@redhat.com>
2019-10-16 09:05:41 +0300

    add test client

---
+++ test/darp_client/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const router = express.Router();
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+app.use('/', router);
+
+router.route('/service').post((req, resp, next) => {
+    console.log(JSON.stringify(req.body, null, 4));
+    let adRequest = req.body;
+    if (adRequest.request.object.metadata.labels.type === "good-service") {
+        resp.send({'isAllowed': true, 'message': 'service has good-services label, allowing to proceed'});
+    } else {
+        resp.send({'isAllowed': false, 'message': 'service doesnt has good-services label, denying to proceed'});
+    }
+});
+
+app.listen(port, () => console.log(`listening on port ${port}!`));
